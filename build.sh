21f256ba18b24883499e911a009c280b5bc36ef7
Jacob McSwain <jacob.a.mcswain@gmail.com>
2025-02-02 01:24:32 -0600

    build: update for my data only

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 set -euo pipefail
 
 rm -rf ./public ./content/gallery ./static/gallery
-mkdir -p ./content/gallery/my-data
-mkdir -p ./content/gallery/other-data
+mkdir -p ./content/gallery/data
 mkdir -p ./static/gallery
 mkdir -p .build-cache
 
@@ -15,8 +14,7 @@ if [ ! -f .build-cache/gallery-${GALLERY_VERSION}.tar.gz ]; then
   curl -fSsL https://github.com/USA-RedDragon/astro.garden-images/releases/download/${GALLERY_VERSION}/gallery.tar.gz -o .build-cache/gallery-${GALLERY_VERSION}.tar.gz
 fi
 tar -xzf .build-cache/gallery-${GALLERY_VERSION}.tar.gz -C ./static/gallery
-mv ./static/gallery/generated/my-data.json data/my.json
-mv ./static/gallery/generated/other-data.json data/other.json
+mv ./static/gallery/generated/data.json data/data.json
 
 # We have a clean gallery directory
 python generate.py
