86bf595241e292bd331a6bcbc4b083bcdd94d2b3
Jacob McSwain <jacob.a.mcswain@gmail.com>
2025-02-02 01:27:43 -0600

    generate: update for only my data

--- generate.py
+++ generate.py
@@ -9,31 +9,26 @@ logging.basicConfig(level=logging.INFO)
 TEMPLATE_FILE = "templates/image.md"
 DEST_DIR = "./content/gallery"
 
-def generate_image_page(gallery, title, text, src, wip=False, annotated=False):
+def generate_image_page(title, text, src, wip=False, annotated=False):
     with open(TEMPLATE_FILE, 'r') as tf:
         template = Template(tf.read())
-        result = template.substitute(gallery=gallery, title=title, text=text, src=src, wip=wip, annotated=annotated)
+        result = template.substitute(title=title, text=text, src=src, wip=wip, annotated=annotated)
         with open(f"{DEST_DIR}/{src}.md", 'w') as df:
             df.write(result)
 
-def generate(gallery):
+def main():
     images = None
-    with open(f"data/{gallery}.json", 'r') as df:
+    with open(f"data/data.json", 'r') as df:
         images = json.load(df)
 
     for image in images:
         generate_image_page(
-            gallery,
             title=image["title"],
             text=image["text"],
             src=image["src"],
             wip=('wip' in image and image["wip"]),
             annotated=('annotated' in image and image["annotated"])
         )
-
-def main():
-    generate("my")
-    generate("other")
 
 if __name__ == "__main__":
     try:
